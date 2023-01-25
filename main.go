ef840a15731b0601d97fe3a7fb85e35ccca5d595
linaro <alain.pernelle@gmail.com>
2023-01-25 10:29:53 +0100

    Ajout main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+/*
+# ===============================================================================================
+# Auteur : Alain Pernelle pour Projet "Home-Security-Project"
+# Date creation : 25/01/2023
+#
+# Description : Gerer les messages de gotify pour activé l'alarme
+#             : Objectif de l'envoi de metric représentant les états de réplication
+#
+# Parametres  :  dans le fichier de configuration
+#             :  paramètres par defaut pour le projet
+#
+#
+#             : Versions
+#             : ========
+# v0.0        : 25/01/2023
+# v0.1        :
+# v0.2        :
+# v0.3        :
+#             : Bugs Issues
+#             : ===========
+#             :
+*/
+
+package main
