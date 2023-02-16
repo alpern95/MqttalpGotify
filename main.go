3c82a8534ac0050c9ec85e22ad71ee3e3d178ed4
linaro <alain.pernelle@gmail.com>
2023-02-16 11:53:38 +0100

    Ajout fonctions

--- main.go
+++ main.go
@@ -16,8 +16,10 @@
 # v0.0        : 25/01/2023
 # v0.1        : 02/02/2023 test lecture message Gotify en utilisant /gotify/go-api-client
 # v0.2        : 07/02/2023 test publish mqtt topic alarme_armee
-#             : utilisation de /https://github.com/lucacasonato/mqtt" 
-# v0.3        :
+#             : utilisation de /https://github.com/lucacasonato/mqtt"
+#             : 
+# v0.3        : 16/02/2023 Ajouter des fonctions pour la lisibilité
+#             : 16/03/2023 creat fonction readappmess & delappmess
 #             : Bugs Issues
 #             : ===========
 #             :
@@ -59,93 +61,43 @@ const (
 
 func main() {
     log.Info("Début")
-    myURL, _ := url.Parse(gotifyURL)
-
-
-    // lire les messages recue
-    client := gotify.NewClient(myURL, &http.Client{}) // ok 
-
-    params := message.NewGetAppMessagesParams() //Ajout App
-    params.ID = 1
-
-    messagesResponse, err := client.Message.GetAppMessages(params,auth.TokenAuth(applicationToken)) // OK
+    // lire les messages recue dans aap 1
+    err := readappmess()
     if err != nil {
-        log.Fatalf("Could not get messages %v", err)
-        return
-    }
-    log.Info("print version en real message ",messagesResponse)
-    log.Info("print typeof version en real message ",reflect.TypeOf(messagesResponse))
+        log.Fatal ("FATAL appel readappmess",err)
+    }else { log.Debug("Main: retour fonc readappmess: Suppression des messages lu  oK")}
 
-    // Faire une fonction  avec les messages gotify en retour.
+    // supprimer les messages lues (faire une fonction)
+    err = delappmess()
+    if err != nil { 
+        log.Fatal ("FATAL appel delappmess",err) 
+    }else { log.Debug("Main : retour fonc delappmess: Suppression des messages lu  oK")}
 
-    // extraire les messages
-    messages := messagesResponse.Payload
-    log.Info("Les messages ",messages)
+    //envoie notification
+}
 
-    log.Info("Les messages all ",messages.Messages)
-    // calcul Taille
-    log.Info("print Calcul du message Paging Size ",len(messages.Messages))
-
-    mess := messages.Messages
-    for _, Messages := range mess {
-        // traitez chaque message ici
-        log.Info("Message ApplicationID:  ",Messages.ApplicationID)
-        //log.Info("Message: ",Messages.Message)
-        // traitez chaque message ici Si message alarme on alors pubalarmearmee()
-        if Messages.Title=="alarme"{
-            //traitement des messages alarme 
-            log.Info("Titre du message: ",Messages.Title)
-            switch Messages.Message{
-            case "off":
-                log.Info("Message case off: ",Messages.Message)
-            case "Off":
-                 pubalarmearmee()
-                log.Info("Message case off: ",Messages.Message)
-                pubalarmearmee()
-            case "on":
-                pubalarmearmee()
-                 log.Info("Message case on: ",Messages.Message)
-            case "On":
-                pubalarmearmee()
-                log.Info("Message case on: ",Messages.Message)
-            default:
-                log.Info("Message case default: ",Messages.Message)
-            }
-        }
-        log.Info("Message à supprimer Apllication ID : ",Messages.ApplicationID)
-        log.Info("Message à supprimer Message ID: ",Messages.ID)
-    }
-
-    // supprimer les messages lues
-    //faire new NewDeleteAppMessagesParams() 
-    //params := message.NewGetAppMessagesParams() //Ajout App
-    //params.ID = 1
-
-    // DeleteAppMessages(params *DeleteAppMessagesParams, authInfo runtime.ClientAuthInfoWriter)
+// ajout pour mqtt
+func delappmess()(error){
+    myURL, _ := url.Parse(gotifyURL)
+    client := gotify.NewClient(myURL, &http.Client{})
     paramdels := message.NewDeleteAppMessagesParams()
     paramdels.ID = 1
     messagesDelResponse, err := client.Message.DeleteAppMessages(paramdels,auth.TokenAuth(applicationToken)) // OK
     if err != nil {
         log.Fatalf("Could not get messages %v", err)
-        return
+        return err
     }
-    // cannot use params (type *message.GetAppMessagesParams) as type *message.DeleteAppMessagesParams
-    log.Info("Message Del ",messagesDelResponse)
-    //pubalarmearmee()
-
-
-    //envoie notification
-
+    log.Info("func delappmess: All Messages Deleted ",messagesDelResponse)
+    return err
 }
 
-// ajout pour mqtt
 func ctx() context.Context {
 	cntx, _ := context.WithTimeout(context.Background(), 1*time.Second)
 	return cntx
 }
 
 // fonction publish arme armée ajouté varible 1 ou 0
-    func pubalarmearmee() {
+    func pubalarmearmee(valeur int)(error) {
         // Publier 1 dans topic alarme_armee  si le mesage est alarme on
         // Publier 0 dans topic alarme_armee  si le mesage est alarme off
         mqttclient, err := mqtt.NewClient(mqtt.ClientOptions{
@@ -163,7 +115,63 @@ func ctx() context.Context {
             log.Fatalf("failed to connect to mqtt server: %v\n", err)
         }else {log.Info("connect to mqtt server: OK %v\n", err)}
 
-    err = mqttclient.PublishJSON(ctx(), "alarme_armee/",1, mqtt.AtLeastOnce)
+    // ajout valeur variable 0 ou 1
+    err = mqttclient.PublishJSON(ctx(), "alarme_armee/",valeur, mqtt.AtLeastOnce)
     if err != nil {
         panic(err)
-    }else {log.Info("Publish mqtt message: OK %v\n", err)}}
+    }else {log.Info("Publish mqtt message: OK %v\n", err)}
+    return err
+}
+
+func readappmess()(error){
+    myURL, _ := url.Parse(gotifyURL)
+    // lire les messages recue
+    client := gotify.NewClient(myURL, &http.Client{}) // ok
+    params := message.NewGetAppMessagesParams() //Ajout App
+    params.ID = 1                               // Id de l'application
+    messagesResponse, err := client.Message.GetAppMessages(params,auth.TokenAuth(applicationToken)) // OK
+    if err != nil {
+        log.Fatalf("Could not get messages %v", err)
+        return err
+    }
+    log.Info("print version en real message ",messagesResponse)
+    log.Info("print typeof version en real message ",reflect.TypeOf(messagesResponse))
+    // extraire les messages
+    messages := messagesResponse.Payload
+    log.Info("Les messages ",messages)
+    log.Info("Les messages all ",messages.Messages)
+    // calcul Taille
+    log.Info("print Calcul du message Paging Size ",len(messages.Messages))
+    mess := messages.Messages
+    for _, Messages := range mess {      // Boucle for pour chaque messages dans l'appli ID 1
+        // traitez chaque message ici
+        log.Info("Message ApplicationID:  ",Messages.ApplicationID)
+        //log.Info("Message: ",Messages.Message)
+        // traitez chaque message ici Si message alarme on alors pubalarmearmee()
+        if Messages.Title=="alarme"{
+            //traitement des messages alarme
+            log.Info("Titre du message: ",Messages.Title)
+            switch Messages.Message{
+            case "off":
+                log.Info("Message case off: ",Messages.Message)
+            case "Off":
+                err = pubalarmearmee(0)
+                if err != nil {
+                  panic(err)
+                }else {log.Info("Publish mqtt message Case Off: OK %v\n", err)} 
+                log.Info("Message case Off: ",Messages.Message)
+            case "on":
+                pubalarmearmee(1)
+                log.Info("Message case on: ",Messages.Message)
+            case "On":
+                pubalarmearmee(1)
+                log.Info("Message case on: ",Messages.Message)
+            default:
+                log.Info("Message case default: ",Messages.Message)
+            }
+        }
+        log.Info("Message à supprimer Apllication ID : ",Messages.ApplicationID)
+        log.Info("Message à supprimer Message ID: ",Messages.ID)
+    }
+	return err
+}
